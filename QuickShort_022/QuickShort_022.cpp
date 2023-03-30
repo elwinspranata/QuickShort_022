aa9596b9a5f9b789d42794fb029c716fde6c2314
elwinspranata <elwinpranata02@gmail.com>
2023-03-30 10:55:31 +0700

    elemnt Quick

--- QuickShort_022/QuickShort_022.cpp
+++ QuickShort_022/QuickShort_022.cpp
@@ -8,12 +8,31 @@ int mov_count = 0; // number of data movements
 int n; 
 
 void input() {
-	while (true) 
-	{ 
+	while (true)
+	{
 		cout << "masukan panjang element array: ";
-		cin >> n; 
+		cin >> n;
 
 		if (n <= 20)
 			break;
 		else
-			cout << "\nMaksimum panjang array adalah 20" << endl; 
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-----------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-----------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+} 
+//swaps the element at index x with the element at index y 
+void swap(int x, int y)
+{
+	int temp; 
+
+	temp = arr[x]; 
+}
